941e79602020e4f3cded8336b57d38e9950c6b93
emilianogastaldo <emiliano0303@gmail.com>
2024-05-23 14:38:39 +0200

    bonus

--- src/esercizio/Main.java
+++ src/esercizio/Main.java
@@ -1,13 +1,26 @@
 package esercizio;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        boolean stop = false;
+    public static void main(String[] args) throws IOException {
         ArrayList<String> wishList = new ArrayList<>();
+        File wishListText = new File("./resources/wish-list.txt");
+        Scanner wishReader = new Scanner(wishListText);
+        while(wishReader.hasNextLine()){
+            String oldWish = wishReader.nextLine();
+            wishList.add(oldWish);
+        }
+        wishReader.close();
+        Scanner scanner = new Scanner(System.in);
+//        Se inizializzo il Writer nel ciclo andrò a riscrivere ogni volta il file.
+        FileWriter wishWriter = new FileWriter(wishListText, true);
+        boolean stop = false;
         while (!stop){
             System.out.println("Che regalo vuoi inserire? Scrivi no per uscire");
             String regalo = scanner.nextLine();
@@ -15,8 +28,11 @@ public class Main {
                 stop = true;
             }else {
                 wishList.add(regalo);
+                wishWriter.write(regalo+"\n");
             }
         }
+        wishWriter.close();
+        scanner.close();
         System.out.println(wishList.toString());
     }
 }
